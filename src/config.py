0ef3fc190aeded8e159ebb0ec6d8e2aeae38d341
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-11-06 21:53:06 +0000

    feat: setup logging parameters

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import DirectoryPath, FilePath
 
+from loguru import logger
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_encoding='utf-8')
@@ -8,5 +10,9 @@ class Settings(BaseSettings):
     data_file_name: FilePath
     model_path: DirectoryPath
     model_name: str
+    log_level: str
 
 settings = Settings()
+
+logger.remove()
+logger.add("app.log", rotation="1 day", retention="7 days", compression="zip", level=settings.log_level)
