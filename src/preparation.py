904c31b5e64af304eeca0b865d79a0ea5dcd50f4
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-11-07 20:47:18 +0000

    fix: * load from db instead of csv * func name enode -> encode * pandas warnings due to indexing

--- src/preparation.py
+++ src/preparation.py
@@ -1,19 +1,22 @@
 import pandas as pd
 import re
-from collection import load_data
+from collection import load_data_from_db
 from loguru import logger
+
+
 def prepare_data():
     logger.info("Preporcessing data pipeline started")
-    data = load_data()
-    data_encoded = enode_cat_cols(data)
+    data = load_data_from_db()
+    data_encoded = encode_cat_cols(data)
     df = parse_garden_col(data_encoded)
 
     return df
 
 
-def enode_cat_cols(data):
+def encode_cat_cols(data):
     cols = ['balcony', 'storage', 'parking', 'furnished', 'garage']
     logger.info("Encoding categorical columns: {cols}")
+    
     return pd.get_dummies(data,
                           columns=cols,
                           drop_first=True,
@@ -22,9 +25,9 @@ def enode_cat_cols(data):
 def parse_garden_col(data):
     logger.info("Parsing garden column")
     for i in range(len(data)):
-        if data.garden[i] == "Not present":
-            data.garden[i] = 0
+        if data.loc[i, "garden"] == "Not present":
+            data.loc[i, "garden"] = 0
         else:
-            data.garden[i] = int(re.findall(r'\d+', data.garden[i])[0])
+            data.loc[i, "garden"] = int(re.findall(r'\d+', data.loc[i, "garden"])[0])
         
     return data
